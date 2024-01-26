0390ed0fd79bf358aa415190332d0187d1a09a2d
Momo <momo5418@outlook.com>
2024-01-26 21:25:22 +0800

    更新 omni_m7332.mk

--- omni_m7332.mk
+++ omni_m7332.mk
@@ -6,15 +6,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from m7332 device
 $(call inherit-product, device/mediatek/m7332/device.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/mediatek/m7332/gsm.mk)
 
 PRODUCT_DEVICE := m7332
 PRODUCT_NAME := omni_m7332
